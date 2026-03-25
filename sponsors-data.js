c7ea85cc3de6216d08e5bc3e2bebe6d92c693eca
Tony Stevenson <pctony@apple.com>
2026-03-25 08:17:02 -0700

    move sponsors logos to images

--- sponsors-data.js
+++ sponsors-data.js
@@ -1,10 +1,10 @@
 const sponsorsData = [
-    { name: "Seabee Construction", logo: "/sponsors/seabee.png", url: "https://builtbyseabee.com" },
-    { name: "Nike Soccer", logo: "/sponsors/nike_soccer_72dpi_200px.png", url: "https://www.nike.com/soccer" },
-    { name: "Rangers Soccer", logo: "/sponsors/rangers-soccer-academy-v2.png", url: "https://www.rangerssoccerschools.com/usa/" },
-    { name: "Tursi Soccer", logo: "/sponsors/tursi_soccer_72dpi_200px.png", url: "https://tursissoccer.com/" },
-    { name: "US Youth Soccer", logo: "/sponsors/us_youth_soccer_72dpi_200px.png", url: "https://www.usyouthsoccer.org/" },
-    { name: "OYSA", logo: "/sponsors/oysa_72dpi_200px.png", url: "https://www.oregonyouthsoccer.org/" }
+    { name: "Seabee Construction", logo: "/images/sponsors/seabee.png", url: "https://builtbyseabee.com" },
+    { name: "Nike Soccer", logo: "/images/sponsors/nike_soccer_72dpi_200px.png", url: "https://www.nike.com/soccer" },
+    { name: "Rangers Soccer", logo: "/images/sponsors/rangers-soccer-academy-v2.png", url: "https://www.rangerssoccerschools.com/usa/" },
+    { name: "Tursi Soccer", logo: "/images/sponsors/tursi_soccer_72dpi_200px.png", url: "https://tursissoccer.com/" },
+    { name: "US Youth Soccer", logo: "/images/sponsors/us_youth_soccer_72dpi_200px.png", url: "https://www.usyouthsoccer.org/" },
+    { name: "OYSA", logo: "/images/sponsors/oysa_72dpi_200px.png", url: "https://www.oregonyouthsoccer.org/" }
 ];
 
 //example usage
